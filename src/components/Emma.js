f9628c0fdb48f66cf56f1eb34af8d6c4a79d64e5
Emma K <emma.kelminson001@umb.edu>
2021-03-15 14:40:33 -0400

    errorsss

--- src/components/Emma.js
+++ src/components/Emma.js
@@ -1,16 +1,71 @@
 import { render } from "@testing-library/react"
 import * as tf from "@tensorflow/tfjs";
 import * as dfd from "danfojs/src/index";
-import raw from "../example_SETUP.SQL";
+import * as lgreg from "ml-logistic-regression";
+import * as prprcss from "ml-preprocess";
 import React from 'react';
+import logo from '../logo.svg';
+import Papa from "papaparse";
+import raw from "../example_SETUP.SQL";
 export default class Emma extends React.Component {
-  state = {
-
-  };
+  constructor(props) {
+    super(props);
+    this.state = {imageSource: logo};
+}
 
   componentDidMount(){};
   componentWillUnmount(){};
   render(){
+    var showFile = async (fileName) => {
+      fileName.preventDefault()
+
+      // read in text file and display it on the console
+      // const textReader = new FileReader()
+      // textReader.onload = async (fileName) => { 
+      //   console.log(fileName.target.result);
+      // };
+      // textReader.readAsText(fileName.target.files[4])
+
+
+      // read in first image and display where logo was using the callback I made
+      // create the image reader and callback to use the result when finished
+     const imageReader = new FileReader()
+      imageReader.onload = async (fileName) => { 
+          console.log(fileName.target.result);
+          this.setState({imageSource: fileName.target.result})
+          };
+      // start the reading of the image
+      imageReader.readAsDataURL(fileName.target.files[6]);
+
+      // this displays the available files from the user upload of cpa_example directory
+      // console.log(fileName.target.files) 
+
+      // parse with the papa, per_object.csv
+      Papa.parse(fileName.target.files[5], 
+      {
+          // use separate thread to not make app too slow
+          worker: true,
+          // blank lines confuse danfo.js so get rid of them
+          skipEmptyLines: true,
+          // per_object.csv has no strings, so we can get a faster speed reading it
+          fastMode: true, 
+          // convert strings to numbers for smaller memory footprint
+          dynamicTyping: true,
+          // call this when the reading is done
+          complete: (results, file) => {
+          console.log("Parsing complete:");
+          console.log(results.data);
+
+          // turn the data into a dataframe
+         const df = new dfd.DataFrame(tf.tensor(results.data));
+          // print the first 5 lines
+         df.head().print();
+          }
+      });        
+      
+  }	
+  
+
 
       var setup_lines = null;
       var create_index = null;
@@ -35,11 +90,8 @@ export default class Emma extends React.Component {
         console.assert(column_names[column_names.length - 1],
              'AreaNormalized_Cytoplasm_AreaShape_Zernike9_9');
         })
-        fetch(raw)
-        .then(r => r.text()) 
-        .then(text =>  {
-          return text.split("\n");
-        })
+   
+        /*
         .then (lines=> {
           dfd.read_csv("https://drive.google.com/file/d/1Ku2oYxF3dYbhSbLo4dGNkTyp7gkS7PFr")
           .then(df => {
@@ -48,7 +100,7 @@ export default class Emma extends React.Component {
           console.log(object_data)
         })
     
-        /*
+   
         dfd.read_csv("../cpa_example/per_object.csv")
         .then ( df => {
           console.log(df.head());
@@ -66,11 +118,31 @@ export default class Emma extends React.Component {
 
         
       
-      return (
+        
+        return (
+            
+            
           <div>
-            <h2>Emma</h2>
-            <p>Emma Stuff</p>
+              <h2>Emma</h2>
+              <p>Stuff of Emma</p>
+
+              <header className="App-header">
+                  <img src={this.state.imageSource} className="App-logo" alt="logo" />
+                  <p>
+                  woo the atom thingy do the rotate woo yea
+                  </p>
+                  
+              <input type="file" onChange = {(fileName) => showFile(fileName)} webkitdirectory="true" mozdirectory="true" msdirectory="true" odirectory="true" directory="true" multiple/>
+                  <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  >
+                  Learn React
+                  </a>
+              </header>
           </div>
-        );
+      )
   };
 }

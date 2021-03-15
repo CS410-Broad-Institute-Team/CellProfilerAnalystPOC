e1d9cd0565a9b80a2affdabcf206a076a1c10750
YahtzeeRage <Yahiya.Hussain001@umb.edu>
2021-03-15 01:23:56 -0400

    papaparse and danfo work now and all in yahiyajs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 import * as tf from "@tensorflow/tfjs";
 import * as dfd from "danfojs/src/index";
 import * as lgreg from "ml-logistic-regression";
 import * as prprcss from "ml-preprocess";
+import Papa from "papaparse";
 
 import EmmaComponent from "./components/Emma"
+import YahiyaComponent from "./components/Yahiya"
 
 import {
   BrowserRouter as Router,
@@ -16,51 +18,12 @@ import {
 } from "react-router-dom";
 
 
-import raw from "./example_SETUP.SQL";
+
 
 function App() {
-  // file read callback
-  var showFile = async (fileName) => {
-    fileName.preventDefault()
-    const reader = new FileReader()
-    reader.onload = async (fileName) => { 
-      const text = (fileName.target.result)
-      console.log(text)
-      alert(text)
-    };
-    reader.readAsText(fileName.target.files[0])
-  }	
-
-  fetch(raw)
-	.then(r => r.text())
-	.then(text => {
-		return text.split("\n");
-	})
-	.then(lines => {
-		console.log(lines.indexOf("CREATE TABLE per_object \("));
-		console.log(lines.indexOf(" PRIMARY KEY  \(ImageNumber,ObjectNumber\)"));
-	})
-
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          woo the atom thingy do the rotate woo yea
-        </p>
-	<input type="file" onChange = {(fileName) => showFile(fileName)} />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      
       <Router>
         <div>
           <ul>
@@ -105,11 +68,7 @@ function App() {
 }
 
 function Yahiya() {
-  return (
-    <div>
-      <h2>Yahiya</h2>
-    </div>
-  );
+  return <YahiyaComponent></YahiyaComponent>
 }
 
 function Alex() {
--- src/components/Yahiya.js
+++ src/components/Yahiya.js
@@ -1,13 +1,112 @@
 import { render } from "@testing-library/react"
+import * as tf from "@tensorflow/tfjs";
+import * as dfd from "danfojs/src/index";
+import * as lgreg from "ml-logistic-regression";
+import * as prprcss from "ml-preprocess";
+import React from 'react';
+import logo from '../logo.svg';
+import Papa from "papaparse";
+// import raw_csv from '../per_object.csv'
+
+// import raw from "./example_SETUP.SQL";
 
 export default class Yahiya extends React.Component {
-    state = {
+    constructor(props) {
+        super(props);
+        this.state = {imageSource: logo};
+    }
+
+    componentDidMount(){
 
     };
-
-    componentDidMount(){};
     componentWillUnmount(){};
     render(){
 
+        // manually fetch and modify string in javascript normally
+        // fetch(raw)
+        // .then(r => r.text())
+        // .then(text => {
+        // 	return text.split("\n");
+        // })
+        // .then(lines => {
+        // 	console.log(lines.indexOf("CREATE TABLE per_object \("));
+        // 	console.log(lines.indexOf(" PRIMARY KEY  \(ImageNumber,ObjectNumber\)"));
+        // })
+            
+    // file read callback
+        var showFile = async (fileName) => {
+            fileName.preventDefault()
+
+            // read in text file and display it on the console
+            // const textReader = new FileReader()
+            // textReader.onload = async (fileName) => { 
+            //   console.log(fileName.target.result);
+            // };
+            // textReader.readAsText(fileName.target.files[4])
+
+
+            // read in first image and display where logo was using the callback I made
+            // create the image reader and callback to use the result when finished
+            const imageReader = new FileReader()
+            imageReader.onload = async (fileName) => { 
+                console.log(fileName.target.result);
+                this.setState({imageSource: fileName.target.result})
+                };
+            // start the reading of the image
+            imageReader.readAsDataURL(fileName.target.files[6]);
+
+            // this displays the available files from the user upload of cpa_example directory
+            // console.log(fileName.target.files) 
+
+            // parse with the papa, per_object.csv
+            Papa.parse(fileName.target.files[5], 
+            {
+                // use separate thread to not make app too slow
+                worker: true,
+                // blank lines confuse danfo.js so get rid of them
+                skipEmptyLines: true,
+                // per_object.csv has no strings, so we can get a faster speed reading it
+                fastMode: true, 
+                // convert strings to numbers for smaller memory footprint
+                dynamicTyping: true,
+                // call this when the reading is done
+                complete: (results, file) => {
+                console.log("Parsing complete:");
+                console.log(results.data);
+
+                // turn the data into a dataframe
+                const df = new dfd.DataFrame(tf.tensor(results.data));
+                // print the first 5 lines
+                df.head().print();
+                }
+            });        
+            
+        }	
+        
+        return (
+            
+            
+            <div>
+                <h2>Yahiya</h2>
+                <p>Yahiya Stuff</p>
+
+                <header className="App-header">
+                    <img src={this.state.imageSource} className="App-logo" alt="logo" />
+                    <p>
+                    woo the atom thingy do the rotate woo yea
+                    </p>
+                    
+                <input type="file" onChange = {(fileName) => showFile(fileName)} webkitdirectory="true" mozdirectory="true" msdirectory="true" odirectory="true" directory="true" multiple/>
+                    <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                    Learn React
+                    </a>
+                </header>
+            </div>
+        )
     };
 }

5f238cd2386c925d34f50a897940f07485738452
YahtzeeRage <Yahiya.Hussain001@umb.edu>
2021-03-18 18:58:07 -0400

    image upload feature

--- src/components/ProofOfConcept.js
+++ src/components/ProofOfConcept.js
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react"
-import React from 'react'
+import React, {useState} from 'react'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid';
@@ -7,11 +7,56 @@ import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 
 import jones from '../jones.jpg'
+import { findRenderedComponentWithType } from "react-dom/test-utils";
+
+
 
 export default class ProofOfConcept extends React.Component {
-    state = {
 
-    };
+    
+    state = {
+        imageSources: new Array(9).fill(jones)
+    }
+
+    async fileReaderPromiseImage(fileName, fileIndex) {
+        return new Promise((resolve, reject)=> {
+            var fr = new FileReader();
+            fr.onload = () => {
+                resolve(fr.result)
+            };
+            fr.readAsDataURL(fileName.target.files[fileIndex])
+        })
+    }
+
+    on_folder_uploaded_callback = function(fileName) {
+        console.log("folder upload done!")
+    
+
+        Promise.all(Array.from({length: 9}, (_,idx)=>
+            this.fileReaderPromiseImage(fileName, idx+6).then(img=>{
+                this.state.imageSources[idx] = img;
+                this.setState({imageSources: this.state.imageSources})
+            }
+        )))
+        .then(()=>console.log("Finished Loading Images"))
+
+        // const imageReader = new FileReader()
+        //     imageReader.onload = async (fileName) => { 
+        //         console.log(fileName.target.result);
+    
+        //         this.state.imageSources[0] = fileName.target.result;
+    
+        //         this.setState({imageSources: this.state.imageSources})
+        //         };
+        //     // start the reading of the image
+    
+    
+    
+        //     imageReader.readAsDataURL(fileName.target.files[6]);
+
+            
+    
+    }
 
     componentDidMount(){};
     componentWillUnmount(){};
@@ -19,6 +64,7 @@ export default class ProofOfConcept extends React.Component {
 
         // your javascript goes here:
         console.log("hi there");
+        console.log(this.state.imageSources)
 
         // JSX goes here:
         return (<div>
@@ -38,21 +84,34 @@ export default class ProofOfConcept extends React.Component {
                     <Button variant="contained"
                             onClick={()=>console.log("Evaluate!")}>Evaluate</Button>    
                 </Grid>
+
+                <Grid key={3} item>
+                    <Button variant="contained" component="label"
+                            onClick={()=>console.log("Upload!")}>
+                        Upload
+                        <input  type="file" 
+                                hidden webkitdirectory="true" 
+                                mozdirectory="true" 
+                                msdirectory="true" 
+                                odirectory="true" 
+                                directory="true" 
+                                multiple 
+                                onChange = {(fileName) => this.on_folder_uploaded_callback(fileName)}
+                                />
+                    </Button>    
+                </Grid>
             </Grid>
 
-            <Container  maxWidth="sm">
-                
-            <GridList cellHeight="auto" cols={3}>
-                {[0,1,2,3,4,5,6,7,8].map((tile) => (
-                <GridListTile key={tile} cols={ 1}>
-                    <Button onClick={()=>console.log(`Click Image: ${tile}!`)}>
-                        <img  width={'100%'} src={jones} alt={"jones"} />
-                    </Button>
-                </GridListTile>
-                ))}
-            </GridList>
-                
-
+            <Container  maxWidth="sm" spacing={0}>    
+                <GridList cellHeight="auto" cols={3}>
+                    {[0,1,2,3,4,5,6,7,8].map((tile) => (
+                    <GridListTile key={tile} cols={ 1} spacing={0}>
+                        <Button onClick={()=>console.log(`Click Image: ${tile}!`)}>
+                            <img  width={'100%'} src={this.state.imageSources[tile]} alt={"jones"} />
+                        </Button>
+                    </GridListTile>
+                    ))}
+                </GridList>
             </Container>
         </div>)
     };

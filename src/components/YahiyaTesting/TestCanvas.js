3ecff9ff43480013bfc49202846888d5bd55e4c7
YahtzeeRage <Yahiya.Hussain001@umb.edu>
2021-03-29 20:31:59 -0400

    showcase how to use canvas

--- src/components/YahiyaTesting/TestCanvas.js
+++ src/components/YahiyaTesting/TestCanvas.js
@@ -29,26 +29,17 @@ const loadImageFromFilePromise = (img_file) => {
 const onFinishUpload_Callback = async function(fileListObject) {
     
     var example_canvas = document.getElementById("example_canvasid");
-    // draw a red rectangle in the canvas I guess
-    var ctx = example_canvas.getContext("2d");
+    var example_ctx = example_canvas.getContext("2d");
 
     const img_file = findFile(fileListObject, "AS_09125_050116000001_A01f00d0.png")
-
     const img = await loadImageFromFilePromise(img_file);
-
     const img_tensor = tf.browser.fromPixels(img)
 
-    console.log(img_file);
-    console.log(img);
-    console.log(img_tensor);
+    var temp_canvas = document.createElement('canvas');
+    await tf.browser.toPixels(img_tensor, temp_canvas );
+    await example_ctx.drawImage(temp_canvas, 0, 0, example_canvas.width, example_canvas.height)
 
-    const img_array = await tf.browser.toPixels(img_tensor );
-    const img_size = 512
-    const img_data = new ImageData(img_array, img_size)
-    ctx.scale(0.25, 0.25);
-
-    ctx.putImageData(img_data, 0, 0);
-    
+    temp_canvas.remove();
     
 }
 
@@ -77,7 +68,7 @@ const app = function() {
             <div id="loss-cont"></div>
             <div id="acc-cont"></div>
             <div id="confusion-matrix"></div>
-            <canvas width={512} height={512} id="example_canvasid"></canvas>
+            <canvas width={270} height={270} id="example_canvasid"></canvas>
             <img id="example_imageid"></img>
         </div>
         

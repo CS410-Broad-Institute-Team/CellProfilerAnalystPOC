1229809b80406268098a1de1df53c0d28a15c1eb
YahtzeeRage <Yahiya.Hussain001@umb.edu>
2021-03-29 22:52:56 -0400

    predict set util

--- src/components/YahiyaTesting/GeneralClassifier.js
+++ src/components/YahiyaTesting/GeneralClassifier.js
@@ -75,7 +75,7 @@ export default class LogisticRegressionClassifier {
 
     
 
-    static basicTrain = async ( training_dataset, feature_count, number_epochs, render_containers=null ) => {
+    static basicTrainset = async ( training_dataset, feature_count, number_epochs, render_containers=null ) => {
         console.log(feature_count)
         const model = LogisticRegressionClassifier.createLogisticRegressionModel(feature_count);
 

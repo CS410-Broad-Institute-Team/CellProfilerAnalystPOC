89279e1bddc0349d7a59416b7575d36ecec7cb35
YahtzeeRage <Yahiya.Hussain001@umb.edu>
2021-04-14 13:52:21 -0400

    imgridmanager test case: fail on mismatched dataurl and datapairs length"

--- src/classes/imGridManager.js
+++ src/classes/imGridManager.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { render } from "@testing-library/react"
-import Elephant from './Elephant.jpg';
-import cheetah from './cheetah.jpg'
-import cat from './cat.jpg'
-import labrador from './labrador.jpg'
-import puggy from './puggy.jpg'
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import _ from "lodash"; 
 
-class imGridManager extends React.Component {
-    render(){
-      function getDataUrl(img) {
+class ImageGridManager {
+
+    constructor(data_pairs, dataurls) {
+
+      
+      if (!data_pairs || !dataurls) {
+          throw new Error("Constructor Error on no data url or data pairs passed in")
+      }
+    }
+
+    static getDataUrlFromImg(img) {
       // Create canvas
       const canvas = document.createElement('canvas');
       const ctx = canvas.getContext('2d');
@@ -35,94 +33,103 @@ class imGridManager extends React.Component {
       // Draw the image
       //ctx.drawImage(img, 0, 0);
 
+    }
+
+    get_number_images (arr) {
+      return data_pairs.length
+    }
+
+
+    get_url_by_ID (index) {
+      return urls[index]
+    }
+
+    set_grid_prop (index, gridType) {
+      if(classifications[index] !== gridType){
+        classifications[index] = gridType
       }
+    }
+  //   render(){
+      
     
-      const urls = [];
-      //construct list of dataUrls
-      urls.push(getDataUrl(Elephant))
-      urls.push(getDataUrl(cheetah))
-      urls.push(getDataUrl(puggy))
-      urls.push(getDataUrl(labrador))
-      urls.push(getDataUrl(cat))
+  //     const urls = [];
+  //     //construct list of dataUrls
+  //     urls.push(imGridManager.getDataUrlFromImg(Elephant))
+  //     urls.push(imGridManager.getDataUrlFromImg(cheetah))
+  //     urls.push(imGridManager.getDataUrlFromImg(puggy))
+  //     urls.push(imGridManager.getDataUrlFromImg(labrador))
+  //     urls.push(imGridManager.getDataUrlFromImg(cat))
       
-      //console.log(urls[0])
+  //     //console.log(urls[0])
 
-      const data_pairs = [{
-          "ImageKey": 2, 
-          "ObjectKey" : 3
-           },
-           {
-            "ImageKey": 3,
-            "ObjectKey":4   
-           },
-           {
-            "ImageKey": 4,
-            "ObkectKey": 5  
-           },
-           {
-            "ImageKey":5,
-            "ObjectKey":6
-           },
-           {
-            "ImageKey": 6,
-            "ObjectKey": 7
-           }
-        ]
+  //     const data_pairs = [{
+  //         "ImageKey": 2, 
+  //         "ObjectKey" : 3
+  //          },
+  //          {
+  //           "ImageKey": 3,
+  //           "ObjectKey":4   
+  //          },
+  //          {
+  //           "ImageKey": 4,
+  //           "ObkectKey": 5  
+  //          },
+  //          {
+  //           "ImageKey":5,
+  //           "ObjectKey":6
+  //          },
+  //          {
+  //           "ImageKey": 6,
+  //           "ObjectKey": 7
+  //          }
+  //       ]
 
-      const classifications = []
-      var size = data_pairs.length
+  //     const classifications = []
+  //     var size = data_pairs.length
       
-      //create classfication array and fill up indices up to size of 
-      // data_pair array
-      for(var i = 0; i < size; i++){
-        classifications.push("unclassified")
-      }
-      //console.log(classifications)
-      const get_url_by_ID = (index) => {
-         return urls[index]
-      }
+  //     //create classfication array and fill up indices up to size of 
+  //     // data_pair array
+  //     for(var i = 0; i < size; i++){
+  //       classifications.push("unclassified")
+  //     }
+  //     //console.log(classifications)
       
-      //test get_url_by_ID function
-      //const val = get_url_by_ID(2)
-      //console.log(val)
+      
+  //     //test get_url_by_ID function
+  //     //const val = get_url_by_ID(2)
+  //     //console.log(val)
     
-      const get_number_images = (arr) => {
-          return data_pairs.length
-      }
+      
 
-      //test get_number_images function
-      //console.log(get_number_images(data_pairs))
+  //     //test get_number_images function
+  //     //console.log(get_number_images(data_pairs))
 
-      const set_grid_prop = (index, gridType) => {
-        if(classifications[index] !== gridType){
-          classifications[index] = gridType
-        }
+      
+  //     set_grid_prop(1, "positive")
+  //     set_grid_prop(2, "negative")
+  //     //console.log(classifications)
 
-      }
-      set_grid_prop(1, "positive")
-      set_grid_prop(2, "negative")
-      //console.log(classifications)
+  //     const get_pairs = (gridType) => {
+  //         const pairs = []
+  //         for(var i = 0; i < classifications.length; i++){
+  //             if(classifications[i] === gridType){
+  //                 pairs.push(data_pairs[i])
+  //             }
+  //         }
+  //         return pairs
+  //     }
 
-      const get_pairs = (gridType) => {
-          const pairs = []
-          for(var i = 0; i < classifications.length; i++){
-              if(classifications[i] === gridType){
-                  pairs.push(data_pairs[i])
-              }
-          }
-          return pairs
-      }
-
-      console.log(get_pairs("unclassified"))
-      return (
-         <div>
-             <p>imGridManager</p>
-         </div>
+  //     console.log(get_pairs("unclassified"))
+  //     return;
+  //     // return (
+  //     //    <div>
+  //     //        <p>imGridManager</p>
+  //     //    </div>
          
-         );
+  //     //    );
  
-   }
+  //  }
 
 }
-export default imGridManager 
+export {ImageGridManager} 
 //ReactDOM.render(<imGridManager />, document.getElementById('root'));
---
+++ src/classes/tests/imGridManager.test.js
@@ -0,0 +1,34 @@
+import {ImageGridManager} from "../imGridManager"
+var assert = require('chai').assert;
+import _ from "lodash";
+
+
+describe('image Grid manager tests', function() {
+
+    describe('constructor tests', function() {
+
+        it('fail on pass nothing', function() {
+            assert.throws(()=>{
+                const m = new ImageGridManager();
+            }, 
+            Error, "Constructor Error on no data url or data pairs passed in")
+            
+        });
+        it('basic construct', function() {
+            const data_pairs = [ {ImageKey:  4, ObjectKey: 5}, {ImageKey: 2, ObjectKey: 8}]
+            const dataurl1 = "https://i.postimg.cc/0yS7m2dV/AS-09125-050116000001-A01f00d2.png"
+            const dataurl2 = "https://i.postimg.cc/0yS7m2dV/AS-09125-050116000001-A01f00d2.png"
+            const dataurls = [dataurl1, dataurl2]
+            const m = new ImageGridManager(data_pairs, dataurls)
+        });
+        it('fail constructor on mismatched data pair and dataurl length', function() {
+            assert.throws(()=>{
+                const data_pairs = [ {ImageKey:  4, ObjectKey: 5}, {ImageKey: 2, ObjectKey: 8}]
+                const dataurl1 = "https://i.postimg.cc/0yS7m2dV/AS-09125-050116000001-A01f00d2.png"
+                const dataurls = [dataurl1]
+                const m = new ImageGridManager(data_pairs, dataurls)
+              }, 
+              Error, "Constructor Error on mismatched data pair and dataurl length")
+        })
+    });
+});

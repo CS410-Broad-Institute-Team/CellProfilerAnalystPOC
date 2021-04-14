10eb352fd80c21d9a6b159430124c19f6f2f08e8
YahtzeeRage <Yahiya.Hussain001@umb.edu>
2021-04-14 14:01:53 -0400

    add imgridmanager usage test: get number of images

--- src/classes/tests/imGridManager.test.js
+++ src/classes/tests/imGridManager.test.js
@@ -19,7 +19,8 @@ describe('image Grid manager tests', function() {
             const dataurl1 = "https://i.postimg.cc/0yS7m2dV/AS-09125-050116000001-A01f00d2.png"
             const dataurl2 = "https://i.postimg.cc/0yS7m2dV/AS-09125-050116000001-A01f00d2.png"
             const dataurls = [dataurl1, dataurl2]
-            const m = new ImageGridManager(data_pairs, dataurls)
+            const IGManager = new ImageGridManager(data_pairs, dataurls)
+            assert.exists(IGManager)
         });
         it('fail constructor on mismatched data pair and dataurl length', function() {
             assert.throws(()=>{
@@ -30,5 +31,19 @@ describe('image Grid manager tests', function() {
               }, 
               Error, "Constructor Error on mismatched data pair and dataurl length")
         })
+    });
+
+    describe('usage tests', function() {
+
+        it('API: gets number of images', function() {
+            const data_pairs = [ {ImageKey:  4, ObjectKey: 5}, {ImageKey: 2, ObjectKey: 8}]
+            const dataurl1 = "https://i.postimg.cc/0yS7m2dV/AS-09125-050116000001-A01f00d2.png"
+            const dataurl2 = "https://i.postimg.cc/0yS7m2dV/AS-09125-050116000001-A01f00d2.png"
+            const dataurls = [dataurl1, dataurl2]
+            const IGManager = new ImageGridManager(data_pairs, dataurls)
+
+            assert.strictEqual(IGManager.get_number_images(), 2)
+        }); 
+
     });
 });

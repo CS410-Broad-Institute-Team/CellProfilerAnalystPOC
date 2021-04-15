0fbf950bb98ac8a56adfe825985aadf11d8cada2
YahtzeeRage <Yahiya.Hussain001@umb.edu>
2021-04-15 19:29:01 -0400

    number strings

--- src/classes/tests/dataprovider.test.js
+++ src/classes/tests/dataprovider.test.js
@@ -24,16 +24,16 @@ describe('Data Provider tests', function() {
             
 
             const object_data = [
-                {"ImageNumber": 1, "ObjectNumber": 1, "f1": 1, "f2": 2, "f3": 5, "Nuclei_Location_CenterX": 1, "Nuclei_Location_CenterY": 3},
-                {"ImageNumber": 2, "ObjectNumber": 2, "f1": 3, "f2": 4, "f3": 6, "Nuclei_Location_CenterX": 2, "Nuclei_Location_CenterY": 5},
-                {"ImageNumber": 3, "ObjectNumber": 3, "f1": 2, "f2": 7, "f3": 3, "Nuclei_Location_CenterX": 5, "Nuclei_Location_CenterY": 4}
+                [1, 1, 1, 2, 5, 1, 3],
+                [2, 2, 3, 4, 6, 2, 5],
+                [3, 3, 2, 7, 3, 5, 4]
             ]
-            const object_columns = ["ImageNumber", "ObjectNumber", "f1", "f2", "f3"]
+            const object_columns = ["ImageNumber", "ObjectNumber", "f1", "f2", "f3", "Nuclei_Location_CenterX", "Nuclei_Location_CenterY"]
 
             const image_data = [
-                {"ImageNumber": 1, "ObjectNumber": 1, "Image_FileNames_Filename_OrigActin": "img1A.png","Image_FileNames_Filename_OrigpH3": "img1P.png","Image_FileNames_Filename_OrigDNA": "img1D.png"},
-                {"ImageNumber": 2, "ObjectNumber": 2, "Image_FileNames_Filename_OrigActin": "img2A.png", "Image_FileNames_Filename_OrigpH3": "img2P.png","Image_FileNames_Filename_OrigDNA": "img2.png"},
-                {"ImageNumber": 3, "ObjectNumber": 3, "Image_FileNames_Filename_OrigActin": "img3A.png", "Image_FileNames_Filename_OrigpH3": "img3P.png","Image_FileNames_Filename_OrigDNA": "img3.png"}
+                [1, 1, "img1A.png", "img1P.png", "img1D.png"],
+                [2, 2, "img2A.png", "img2P.png", "img2D.png"],
+                [3, 3, "img3A.png", "img3P.png", "img3D.png"]
             ]
             const image_columns = ["ImageNumber", "ObjectNumber", "Image_FileNames_Filename_OrigActin", "Image_FileNames_Filename_OrigpH3", "Image_FileNames_Filename_OrigDNA"]
 
@@ -56,18 +56,17 @@ describe('Data Provider tests', function() {
         it('basic get image filenames', function() {
             
             
-
             const object_data = [
-                {"ImageNumber": 1, "ObjectNumber": 1, "f1": 1, "f2": 2, "f3": 5, "Nuclei_Location_CenterX": 1, "Nuclei_Location_CenterY": 3},
-                {"ImageNumber": 2, "ObjectNumber": 2, "f1": 3, "f2": 4, "f3": 6, "Nuclei_Location_CenterX": 2, "Nuclei_Location_CenterY": 5},
-                {"ImageNumber": 3, "ObjectNumber": 3, "f1": 2, "f2": 7, "f3": 3, "Nuclei_Location_CenterX": 5, "Nuclei_Location_CenterY": 4}
+                [1, 1, 1, 2, 5, 1, 3],
+                [2, 2, 3, 4, 6, 2, 5],
+                [3, 3, 2, 7, 3, 5, 4]
             ]
-            const object_columns = ["ImageNumber", "ObjectNumber", "f1", "f2", "f3"]
+            const object_columns = ["ImageNumber", "ObjectNumber", "f1", "f2", "f3", "Nuclei_Location_CenterX", "Nuclei_Location_CenterY"]
 
             const image_data = [
-                {"ImageNumber": 1, "ObjectNumber": 1, "Image_FileNames_Filename_OrigActin": "img1A.png","Image_FileNames_Filename_OrigpH3": "img1P.png","Image_FileNames_Filename_OrigDNA": "img1D.png"},
-                {"ImageNumber": 2, "ObjectNumber": 2, "Image_FileNames_Filename_OrigActin": "img2A.png", "Image_FileNames_Filename_OrigpH3": "img2P.png","Image_FileNames_Filename_OrigDNA": "img2.png"},
-                {"ImageNumber": 3, "ObjectNumber": 3, "Image_FileNames_Filename_OrigActin": "img3A.png", "Image_FileNames_Filename_OrigpH3": "img3P.png","Image_FileNames_Filename_OrigDNA": "img3.png"}
+                [1, 1, "img1A.png", "img1P.png", "img1D.png"],
+                [2, 2, "img2A.png", "img2P.png", "img2D.png"],
+                [3, 3, "img3A.png", "img3P.png", "img3D.png"]
             ]
             const image_columns = ["ImageNumber", "ObjectNumber", "Image_FileNames_Filename_OrigActin", "Image_FileNames_Filename_OrigpH3", "Image_FileNames_Filename_OrigDNA"]
 
@@ -97,18 +96,17 @@ describe('Data Provider tests', function() {
         it('basic get image filenames', function() {
             
             
-
             const object_data = [
-                {"ImageNumber": 1, "ObjectNumber": 1, "f1": 1, "f2": 2, "f3": 5, "Nuclei_Location_CenterX": 1, "Nuclei_Location_CenterY": 3},
-                {"ImageNumber": 2, "ObjectNumber": 2, "f1": 3, "f2": 4, "f3": 6, "Nuclei_Location_CenterX": 2, "Nuclei_Location_CenterY": 5},
-                {"ImageNumber": 3, "ObjectNumber": 3, "f1": 2, "f2": 7, "f3": 3, "Nuclei_Location_CenterX": 5, "Nuclei_Location_CenterY": 4}
+                ["1", "1", "1", "2", "5", "1", "3"],
+                ["2", "2", "3", "4", "6", "2", "5"],
+                ["3", "3", "2", "7", "3", "5", "4"]
             ]
-            const object_columns = ["ImageNumber", "ObjectNumber", "f1", "f2", "f3"]
+            const object_columns = ["ImageNumber", "ObjectNumber", "f1", "f2", "f3", "Nuclei_Location_CenterX", "Nuclei_Location_CenterY"]
 
             const image_data = [
-                {"ImageNumber": 1, "ObjectNumber": 1, "Image_FileNames_Filename_OrigActin": "img1A.png","Image_FileNames_Filename_OrigpH3": "img1P.png","Image_FileNames_Filename_OrigDNA": "img1D.png"},
-                {"ImageNumber": 2, "ObjectNumber": 2, "Image_FileNames_Filename_OrigActin": "img2A.png", "Image_FileNames_Filename_OrigpH3": "img2P.png","Image_FileNames_Filename_OrigDNA": "img2.png"},
-                {"ImageNumber": 3, "ObjectNumber": 3, "Image_FileNames_Filename_OrigActin": "img3A.png", "Image_FileNames_Filename_OrigpH3": "img3P.png","Image_FileNames_Filename_OrigDNA": "img3.png"}
+                ["1", "1", '"img1A.png"', '"img1P.png"', '"img1D.png"'],
+                ["2", "2", '"img2A.png"', '"img2P.png"', '"img2D.png"'],
+                ["3", "3", '"img3A.png"', '"img3P.png"', '"img3D.png"']
             ]
             const image_columns = ["ImageNumber", "ObjectNumber", "Image_FileNames_Filename_OrigActin", "Image_FileNames_Filename_OrigpH3", "Image_FileNames_Filename_OrigDNA"]
 
@@ -139,18 +137,17 @@ describe('Data Provider tests', function() {
         it('basic get random cell pairs', function() {
             
             
-
             const object_data = [
-                {"ImageNumber": 1, "ObjectNumber": 1, "f1": 1, "f2": 2, "f3": 5, "Nuclei_Location_CenterX": 1, "Nuclei_Location_CenterY": 3},
-                {"ImageNumber": 2, "ObjectNumber": 2, "f1": 3, "f2": 4, "f3": 6, "Nuclei_Location_CenterX": 2, "Nuclei_Location_CenterY": 5},
-                {"ImageNumber": 3, "ObjectNumber": 3, "f1": 2, "f2": 7, "f3": 3, "Nuclei_Location_CenterX": 5, "Nuclei_Location_CenterY": 4}
+                ["1", "1", "1", "2", "5", "1", "3"],
+                ["2", "2", "3", "4", "6", "2", "5"],
+                ["3", "3", "2", "7", "3", "5", "4"]
             ]
-            const object_columns = ["ImageNumber", "ObjectNumber", "f1", "f2", "f3"]
+            const object_columns = ["ImageNumber", "ObjectNumber", "f1", "f2", "f3", "Nuclei_Location_CenterX", "Nuclei_Location_CenterY"]
 
             const image_data = [
-                {"ImageNumber": 1, "ObjectNumber": 1, "Image_FileNames_Filename_OrigActin": "img1A.png","Image_FileNames_Filename_OrigpH3": "img1P.png","Image_FileNames_Filename_OrigDNA": "img1D.png"},
-                {"ImageNumber": 2, "ObjectNumber": 2, "Image_FileNames_Filename_OrigActin": "img2A.png", "Image_FileNames_Filename_OrigpH3": "img2P.png","Image_FileNames_Filename_OrigDNA": "img2.png"},
-                {"ImageNumber": 3, "ObjectNumber": 3, "Image_FileNames_Filename_OrigActin": "img3A.png", "Image_FileNames_Filename_OrigpH3": "img3P.png","Image_FileNames_Filename_OrigDNA": "img3.png"}
+                ["1", "1", '"img1A.png"', '"img1P.png"', '"img1D.png"'],
+                ["2", "2", '"img2A.png"', '"img2P.png"', '"img2D.png"'],
+                ["3", "3", '"img3A.png"', '"img3P.png"', '"img3D.png"']
             ]
             const image_columns = ["ImageNumber", "ObjectNumber", "Image_FileNames_Filename_OrigActin", "Image_FileNames_Filename_OrigpH3", "Image_FileNames_Filename_OrigDNA"]
 
